ba7143d3954747884f2c78167372bdac4bec1bc9
Habilu6 <michaelhabilu6@gmail.com>
2025-02-20 18:08:57 -0800

    Add changes to dashboard and package-lock

---
+++ money-dashboard/src/main/java/com/example/money/dashboard/MoneyDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.money.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyDashboardApplication.class, args);
+	}
+
+}
---
+++ money-dashboard/src/test/java/com/example/money/dashboard/MoneyDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.money.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
